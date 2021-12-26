5e5338f0ac444cc5c13aabf1c1a71d4afb8761e9
Nadaalbalawi <nada14040@hotmail.com>
2021-12-26 16:47:49 +0300

    D-2021-12-26 Creating the Onboarding Screen

--- catchBullying/ViewControllers/DoctorLoginViewController.swift
+++ catchBullying/ViewControllers/DoctorLoginViewController.swift
@@ -99,3 +99,4 @@ class DoctorLoginViewController: UIViewController {
     
   }
 }
+
--- catchBullying/ViewControllers/PsicologistQuestionsViewController.swift
+++ catchBullying/ViewControllers/PsicologistQuestionsViewController.swift
@@ -8,11 +8,35 @@
 import UIKit
 
 class PsicologistQuestionsViewController: UIViewController {
+  var currentQuestionIndex: Int = 0
+  var noCorrect = 0
+  
+  let arryPsicologistQuestions : [PsicologistQuestions] = [
+    PsicologistQuestions(question: "Gender", answers:["Male" , "Female" ,"Other"]),
+    PsicologistQuestions(question: "Do you currentiy have a job?", answers:["yes" , "No" ,"prefer not answer"]),
+  PsicologistQuestions(question: "Have you taken a psychiatric medication befor?", answers: ["yes" , "No" ,"prefer not answer"]),
+  PsicologistQuestions(question: "Does your family have a history of psychiatrist counseling?", answers: ["yes" , "No" ,"prefer not answer"]),
+  PsicologistQuestions(question: "Were you subjected to domestic violence (verbal , psychological, physical, sexual , negligence ...)?", answers: ["yes" , "No" ,"prefer not answer"]),
+  PsicologistQuestions(question: "Do you think of hurting yourself or have suicidal thoughts ?", answers: ["yes" , "No" ,"prefer not answer"]),
+  PsicologistQuestions(question: "Do you plan on committing suicide ??", answers: ["yes" , "No" ,"prefer not answer"])
+  
+  ]
+  
+  
+  
+  @IBOutlet weak var questionLabel: UILabel!
+  @IBOutlet weak var yesButton: UIButton!
+  @IBOutlet weak var noButton: UIButton!
+  @IBOutlet weak var otherButton: UIButton!
+  @IBOutlet weak var nextButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.setHidesBackButton(true, animated: false)
+      questionLabel.text=arryPsicologistQuestions[currentQuestionIndex].question
+      configureButtons()
     }
+  
    
   @IBAction func nextButton(_ sender: Any) {
     let vc = storyboard?.instantiateViewController(withIdentifier: "loginuser")
@@ -20,4 +44,31 @@ class PsicologistQuestionsViewController: UIViewController {
   
   
 }
+ 
+  
+  @IBAction func answuer(_ sender: Any) {
+  }
+  
+  
+  
+  
+  func configureButtons() {
+    yesButton.layer.cornerRadius = 20
+    yesButton.layer.borderWidth = 3
+    yesButton.layer.borderColor = CGColor(red: 225, green:255, blue: 255, alpha: 1)
+    
+    noButton.layer.cornerRadius = 20
+    noButton.layer.borderWidth = 3
+    noButton.layer.borderColor = CGColor(red: 225, green:255, blue: 255, alpha: 1)
+    
+    otherButton.layer.cornerRadius = 20
+    otherButton.layer.borderWidth = 3
+    otherButton.layer.borderColor = CGColor(red: 225, green:255, blue: 255, alpha: 1)
+    
+    
+  }
+  
+
+  
+  
 }
